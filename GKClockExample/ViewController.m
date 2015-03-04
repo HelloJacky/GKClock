6fe7b63374bbaf1c791b27f67a72e661154cc1d4
Jacky Ma <hellomgk@gmail.com>
2015-03-04 16:37:37 +0800

    fix

--- GKClockExample/ViewController.m
+++ GKClockExample/ViewController.m
@@ -21,15 +21,15 @@
     self.view.backgroundColor = [UIColor blackColor];
     
     self.clock = [[GKClock alloc] init];
-    self.clock.layer.borderColor = [UIColor yellowColor].CGColor;
-//    self.clock.layer.borderWidth = 1.0f;
     self.clock.frame = CGRectMake(10.f, 0.f, self.view.frame.size.width - 20.f, self.view.frame.size.height - 100.f);
-//    self.clock.minuteHandColor = [UIColor yellowColor];
-//    self.clock.hourHandColor = [UIColor yellowColor];
-//    self.clock.secondHandColor = [UIColor yellowColor];
-//    self.clock.clockBorderColor = [UIColor yellowColor];
-//    self.clock.momentAttribute = @{NSFontAttributeName : [UIFont fontWithName:@"American Typewriter" size:35],
-//                                   NSForegroundColorAttributeName : [UIColor yellowColor]};
+    
+    self.clock.layer.borderColor = [UIColor yellowColor].CGColor;
+    self.clock.minuteHandColor = [UIColor yellowColor];
+    self.clock.hourHandColor = [UIColor yellowColor];
+    self.clock.secondHandColor = [UIColor yellowColor];
+    self.clock.clockBorderColor = [UIColor yellowColor];
+    self.clock.momentAttribute = @{NSFontAttributeName : [UIFont fontWithName:@"American Typewriter" size:35],
+                                   NSForegroundColorAttributeName : [UIColor yellowColor]};
     [self.view addSubview:self.clock];
     [self.clock start];
     
