1e5f6ff0f61774c0dcbb57457f532906265acb4d
Jacky Ma <hellomgk@gmail.com>
2015-08-05 12:02:17 +0800

    fix

---
+++ GKClockExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GKClockExample
+//
+//  Created by Jacky on 15/2/10.
+//  Copyright (c) 2015年 Jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GKClockExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GKClockExample
+//
+//  Created by Jacky on 15/2/10.
+//  Copyright (c) 2015年 Jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
